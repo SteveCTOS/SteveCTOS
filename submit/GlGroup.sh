fa4d847f3888333045d871f05f2841be5b62127e
Steve Christensen <Steve@Christensen.co.za>
2015-10-03 16:04:26 +0200

    Fixing final Submits

--- submit/GlGroup.sh
+++ submit/GlGroup.sh
@@ -1,11 +1,8 @@
 # Enter ALL to get All as $1 the companies renamed to the Month below.
 # Enter a Month Name like Jun, Aug etc. as $2 input.
-<<<<<<< HEAD
 #example  Submit MainCont GlGroupReports.Sub ALL Jun
-=======
 # example from the command line, run the following:
 #  GlGroup.sh ALL Jun
->>>>>>> 59def9fcb1400b8435a45747be4fdd1d3d816821
 Submit MainCont GlGroupReports.Sub
 #Submit MainCont GlMonthTemp
 
--- submit/GlMonthly.sh
+++ submit/GlMonthly.sh
@@ -1,12 +1,8 @@
 # Enter ALL to get All as $1 the companies renamed to the Month below.
 # Enter a Month Name like Jun, Aug etc. as $2 input.
-<<<<<<< HEAD
 #for example    Submit MainCont GlMonthly.Sub ALL Jun
-=======
 # example.  From the command line run the following:
 # GlMonthly.sh ALL Jun
-
->>>>>>> 59def9fcb1400b8435a45747be4fdd1d3d816821
 Submit MainCont GlMonthly.Sub
 #Submit MainCont GlMonthTemp
 
