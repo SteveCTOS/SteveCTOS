f775e26e09fa477c0c8e1e8f9cbb91ca472aeae2
ctools <LinuxServer@christensen.co.za>
2015-11-16 10:51:20 +0200

    fixed

--- submit/Auto07.sh
+++ submit/Auto07.sh
@@ -1,7 +1,7 @@
 Submit MainCont AutoAllocations.Sub07
 
-lp -d MP140 /ctools/spl/sfj-aa1
-lp -d MP140 /ctools/spl/sfj-aa2
+lp -d SFJ-Spl /ctools/spl/sfj-aa1
+lp -d SFJ-Spl /ctools/spl/sfj-aa2
 
 
 echo COMPANY7 RUNNING AND PRINTING OF AUTO-ALLOCATIONS COMPLETE
