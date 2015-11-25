12eb83812264573791b25bc863f225227d5ba637
ctools <LinuxServer@christensen.co.za>
2015-11-25 11:10:18 +0200

    fixed

--- submit/Auto03.sh
+++ submit/Auto03.sh
@@ -1,7 +1,7 @@
 Submit MainCont AutoAllocations.Sub03
 
-lp -d MP140 /ctools/spl/ctn-aa1
-lp -d MP140 /ctools/spl/ctn-aa2
+lp -d CTN-Spl /ctools/spl/ctn-aa1
+lp -d CTN-Spl /ctools/spl/ctn-aa2
 
 
 echo COMPANY3 RUNNING AND PRINTING OF AUTO-ALLOCATIONS COMPLETE
--- submit/Backup06.sh
+++ submit/Backup06.sh
@@ -1,29 +1,29 @@
 echo Your Daily Backup & fax Sending is in Process,  Please be patient... 
 
-mkdir /media/ctools/BACKUPUSB/data01
-mkdir /media/ctools/BACKUPUSB/data02
-mkdir /media/ctools/BACKUPUSB/data03
-mkdir /media/ctools/BACKUPUSB/data04
-mkdir /media/ctools/BACKUPUSB/data05
-mkdir /media/ctools/BACKUPUSB/data06
-mkdir /media/ctools/BACKUPUSB/data07
-mkdir /media/ctools/BACKUPUSB/data08
-mkdir /media/ctools/BACKUPUSB/data09
-mkdir /media/ctools/BACKUPUSB/data10
-mkdir /media/ctools/BACKUPUSB/data11
-mkdir /media/ctools/BACKUPUSB/data12
+mkdir /media/ctools/USB/data01
+mkdir /media/ctools/USB/data02
+mkdir /media/ctools/USB/data03
+mkdir /media/ctools/USB/data04
+mkdir /media/ctools/USB/data05
+mkdir /media/ctools/USB/data06
+mkdir /media/ctools/USB/data07
+mkdir /media/ctools/USB/data08
+mkdir /media/ctools/USB/data09
+mkdir /media/ctools/USB/data10
+mkdir /media/ctools/USB/data11
+mkdir /media/ctools/USB/data12
 
-cp /ctools/data01/* /media/ctools/BACKUPUSB/data01
-cp /ctools/data02/* /media/ctools/BACKUPUSB/data02
-cp /ctools/data03/* /media/ctools/BACKUPUSB/data03
-cp /ctools/data05/* /media/ctools/BACKUPUSB/data05
-cp /ctools/data06/* /media/ctools/BACKUPUSB/data06
-cp /ctools/data07/* /media/ctools/BACKUPUSB/data07
-cp /ctools/data08/* /media/ctools/BACKUPUSB/data08
-cp /ctools/data09/* /media/ctools/BACKUPUSB/data09
-cp /ctools/data10/* /media/ctools/BACKUPUSB/data10
-cp /ctools/data11/* /media/ctools/BACKUPUSB/data11
-cp /ctools/data12/* /media/ctools/BACKUPUSB/data12
+cp /ctools/data01/* /media/ctools/USB/data01
+cp /ctools/data02/* /media/ctools/USB/data02
+cp /ctools/data03/* /media/ctools/USB/data03
+cp /ctools/data05/* /media/ctools/USB/data05
+cp /ctools/data06/* /media/ctools/USB/data06
+cp /ctools/data07/* /media/ctools/USB/data07
+cp /ctools/data08/* /media/ctools/USB/data08
+cp /ctools/data09/* /media/ctools/USB/data09
+cp /ctools/data10/* /media/ctools/USB/data10
+cp /ctools/data11/* /media/ctools/USB/data11
+cp /ctools/data12/* /media/ctools/USB/data12
 
 Submit MainCont TapeBackupFax.Sub01
 
