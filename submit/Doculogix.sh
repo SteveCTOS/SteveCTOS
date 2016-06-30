63990271d106f29c2ec0327802a45beec55117e1
ctools <LinuxServer@christensen.co.za>
2016-06-30 17:06:48 +0200

    fixed

--- submit/Doculogix.sh
+++ submit/Doculogix.sh
@@ -11,19 +11,19 @@ cd /ctools/ctj
 cp dbprintco01 /ctools/image/InCo01
 cp drlaserco01 /ctools/image/StCo01
 cp drlaserco01 /ctools/image/CtjStat
-cp dremailco01 /ctools/estate/$1Co01
+cp dremailco01 /ctools/image/$1Co01
 
 cd /ctools/grc
 cp dbprintco02 /ctools/image/InCo02
 cp drlaserco02 /ctools/image/StCo02
 cp drlaserco02 /ctools/image/GrcStat
-cp dremailco02 /ctools/estate/$1Co02
+cp dremailco02 /ctools/image/$1Co02
 
 cd /ctools/ctn
 cp dbprintco03 /ctools/image/InCo03
 cp drlaserco03 /ctools/image/StCo03
 cp drlaserco03 /ctools/image/CtnStat
-cp dremailco03 /ctools/estate/$1Co03
+cp dremailco03 /ctools/image/$1Co03
 
 cd /ctools/orx
 cp dbprintco05 /ctools/image/InCo05
@@ -33,10 +33,10 @@ cd /ctools/sfj
 cp dbprintco07 /ctools/image/InCo07
 cp drlaserco07 /ctools/image/StCo07
 cp drlaserco07 /ctools/image/SfjStat
-cp dremailco07 /ctools/estate/$1Co07
+cp dremailco07 /ctools/image/$1Co07
 
 cd /ctools/csc
 cp dbprintco08 /ctools/image/InCo08
 cp drlaserco08 /ctools/image/StCo08
 cp drlaserco08 /ctools/image/CscStat
-cp dremailco08 /ctools/estate/$1Co08
+cp dremailco08 /ctools/image/$1Co08
