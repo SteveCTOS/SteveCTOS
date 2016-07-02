1b2c3438b89800b1eb767ea8724d8b759c57a0fd
Steve Christensen <Steve@Christensen.co.za>
2016-07-02 16:28:47 +0200

    fixed

--- submit/Pay2Rename.sh
+++ submit/Pay2Rename.sh
@@ -1,3 +1,5 @@
+# *** THIS PROCESS MUST BE RUN ON THE LINUX SERVER ONLY *****
+
 # Enter ALL to get All as $1 is all the companies renamed to the Month below.
 # Enter a Month Name like Jun16, Aug16 etc. as $2 input.
 # example.  From the command line run the following:
