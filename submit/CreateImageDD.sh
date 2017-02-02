effc313fb4a1b125d315da9cbe32278691a9d7bb
ctools <LinuxServer@christensen.co.za>
2017-02-02 10:04:29 +0200

    fixed

---
+++ submit/CreateImageDD.sh
@@ -0,0 +1,7 @@
+#Script file used for creating an image onto a USB drive of the 240GB SSD (Main)
+
+echo Creating an Image file of the main SSD......
+
+sudo dd bs=64K if=/dev/sda of=/dev/sdc
+
+echo Creation of Backup Image done .....
