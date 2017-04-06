6f4e0d249ba82064b305c7fc29b15bd72b428a65
ctools <LinuxServer@christensen.co.za>
2017-04-06 12:44:14 +0200

    new script files

---
+++ submit/Pay4Rename.sh
@@ -0,0 +1,9 @@
+# **COPY THE FILES FROM USB TO /ctools/temp/ BEFORE RUNNING THIS SCRIPT**
+
+# Enter ALL to get All as $1 is all the companies renamed to the Month below.
+# Enter a Month Name like Jun16, Aug16 etc. as $2 input.
+# example.  From the command line run the following:
+# Pay4Rename.sh ALL Jun16
+
+python renamewmans.py $1 $2
+echo   The rename of all COID WMans files Run Successfully 
---
+++ submit/renamewmans.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# The renaming of W/Mans Comp 'COID' Files to 'WMans' & add in the month & year for filing
+import glob, os, sys
+comps = ['CTJ', 'GRC', 'CTN', 'ORX', 'SFJ', 'CSC', 'SFI', 'QTM', 'HKY', 'KRS']
+months = ['Feb16', 'Feb17', 'FEb18', 'Feb19', 'Feb20']
+comp = sys.argv[1]
+month = sys.argv[2]
+def rename (comp, month):
+  for file in glob.glob('/ctools/temp/%sCOID1.6*' % (comp)):
+    file2 = file.replace('%sCOID1.6' % (comp), '%sWMans%s' % (month, comp))
+    os.rename(file, file2)
+if month in months and comp == 'ALL':
+  for comp in comps:
+	  rename(comp, month)
+elif comp in comps and month in months:
+  rename(comp, month)
+else:
+  print sys.argv
+  print 'Please supply a valid Company (or ALL) & Valid Month Name'
