d2d0aa38ca7fd074c7abb8575a2f78b4f0def620
ctools <LinuxServer@christensen.co.za>
2020-06-30 16:13:43 +0200

    updates committed

---
+++ submit/DrAgeTotals.sh
@@ -0,0 +1,10 @@
+# Enter ALL to get All as $1 is all the companies renamed to the Month below.
+# Enter a Month Name like Jun, Aug etc. as $2 input.
+#for example    Submit MainCont GlMonthly.Sub ALL Jun
+# example.  From the command line run the following:
+# GlMonthly.sh ALL Jun
+Submit MainCont DrAgeTotals.Sub
+
+
+echo   CT Group DrAgeTotals ALL Run Successfully 
+
